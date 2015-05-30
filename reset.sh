2bfce34edd257c6d193b4544b5c58940b006a417
brianoflan <brian.flannery@gmail.com>
2015-05-30 14:24:21 +0000

    d20150530 T0822MDT updated for non-Cygwin build, still broken

--- reset.sh
+++ reset.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-storTierPrefix=storTier ;
+if [[ -z $tier_res ]] ; then
+  export tier_res=. ;
+fi ;
 
-DEBUG=2 ;
+# DEBUG=2 ;
 thisScript=$0 ;
 thisDir=$(dirname $thisScript) ;
 if [[ -z $thisDir || "$thisDir" == "." ]] ; then
   pwdX=$(pwd) ;
-  if [[ $DEBUG -gt 0 ]] ; then
+  if [[ $DEBUG -gt 1 ]] ; then
     echo "Updating thisDir from '$thisDir' to '$pwdX'." ;
   fi ;
   thisDir="$pwdX" ;
@@ -20,51 +22,14 @@ if [[ $DEBUG -gt 1 ]] ; then
   done 1>&2 ;
 fi ;
 
-fo=find_ls_previous_structure.txt ;
-fof="$thisDir/$fo" ;
-if [[ -f "$fof" ]] ; then
-  rm -rf "$fof" ;
-fi ;
-find "$thisDir" -path .git -prune -o -exec ls -ld {} \; > /tmp/$fo ;
-mv /tmp/$fo "$thisDir/" ;
 
-# echo "find $thisDir/.old" ;
-# find $thisDir/.old ;
-if [[ -d "$thisDir/.old" ]] ; then
-  rm -rf "$thisDir/.old"/* 2>/dev/null ;
-fi ;
-mkdir "$thisDir/.old" 2>/dev/null ;
-# echo ;
-# echo "find $thisDir/.old" ;
-# find $thisDir/.old ;
-# echo ;
 
-x=`ls -d "$thisDir"/$storTierPrefix* ` ;
-if [[ $x ]] ; then
-  if [[ $storTierPrefix ]] ; then
-    mv "$thisDir"/$storTierPrefix* "$thisDir/.old/" ;
-  fi ;
-fi ;
+# mk_find_file "-exec ls -ld {} \\;" "find_ls_ld_previous_structure.txt" ;
+# mk_find_file "-print" "find_previous_structure.txt" ;
 
-for n in 1 2 3 4 5 6 7 8 9 ; do
-  mkdir "$thisDir/$storTierPrefix$n" ;
-done ;
-mkdir -p "$thisDir"/${storTierPrefix}1/abc ;
-mkdir -p "$thisDir"/${storTierPrefix}2/def/abc ;
-mkdir -p "$thisDir"/${storTierPrefix}3/def ;
-dd if=/dev/zero of="$thisDir"/${storTierPrefix}1/abc/file1.txt bs=1024 count=1127
-dd if=/dev/zero of="$thisDir"/${storTierPrefix}2/def/abc/file2.txt bs=1024 count=1127
-dd if=/dev/zero of="$thisDir"/${storTierPrefix}3/def/file3.txt bs=1024 count=11027
+resetTiers ;
 
-touch "$thisDir"/${storTierPrefix}1/abc/abc.txt ;
-touch "$thisDir"/${storTierPrefix}2/def/abc/def.abc.txt ;
-touch "$thisDir"/${storTierPrefix}2/def/def.txt ;
-
-touch -d '2010-04-03' "$thisDir"/${storTierPrefix}1/abc/abc.old.txt ;
-touch -d '2010-04-03' "$thisDir"/${storTierPrefix}2/def/abc/def.abc.old.txt ;
-touch -d '2010-04-03' "$thisDir"/${storTierPrefix}2/def/def.old.txt ;
-
-cd "$thisDir" ;
-find ./$storTierPrefix* > ./find_storTier.txt ;
+# cd "$thisDir" ;
+# find ./$storTierPrefix* > ./find_storTier.txt ;
 
 #
